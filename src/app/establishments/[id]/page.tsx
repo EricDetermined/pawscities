1c88934bfb2259cda695634caf3057a29f93c2f9
EricDetermined <eric@ericdetermined.com>
2026-02-06 19:19:52 -0800

    fix: stub page.tsx

--- src/app/establishments/[id]/page.tsx
+++ src/app/establishments/[id]/page.tsx
@@ -1,13 +1,1 @@
-import React from 'react';
-
-interface EstablishmentId { 
-	 params: {id: string}
-}
-
-export default async function EstablishmentDetail({ params }: EstablishmentId) {
-  return (
-    <div className="p-4">
-      <h1>Establishment {params.id}</h1>
-    </div>
-  );
-}
+export default function Page() { return <div>Coming soon</div>; }
